23e1dcafc687199339cf447ba5e53edd5d40b822
Cynthia Nwakaeme <nwakaemecynthia@gmail.com>
2021-03-01 00:02:13 +0100

    retirving code on chaallenge 1

---
+++ challenge1.js
@@ -0,0 +1,11 @@
+function chemicalCompound(formula) {
+    formula = formula(/[A-Z][a-z]*|\d+|[()]/g);
+    let temp = [];
+
+    if (formular.match){
+        for (let i = 0; i < formula.length; i++) {
+            
+         }
+    }
+    
+}
