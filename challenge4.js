9a5570df32834803b6d8fb704bcbf1f016c1facc
Victor-Aienobe <viktornobe@gmail.com>
2021-02-28 22:39:03 +0100

    made modifications

--- challenge4.js
+++ challenge4.js
@@ -1,11 +1,19 @@
 let steps = (arr) => {
+  // DECLARATION OF VARIABLES
     let step = 0, currentPos = 0, maxPos = 0;
+  // step = number of jumps made
+  //currentPos = farthest position that can be reached from the previous step
+  //maxPos= farthest position that can be reached from the present index position
  
     for (let i = 0; i < arr.length - 1; i++) {
        maxPos = Math.max(i + arr[i], maxPos);
      if (i == currentPos) {
         step++;
         currentPos = maxPos;
+        // a break statement to exit the loop if the last item in the array has been reached
+        if(maxPos >= arr.length - 1) {
+          break;
+        }
       }
     }
     return step;
